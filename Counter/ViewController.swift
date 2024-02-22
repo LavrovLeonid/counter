ade2c1a770fb905aeb596b5aff2a2ee19bfa95d6
Lavrov Leonid <l.lavrov@kari.com>
2024-02-22 18:36:42 +0500

    feat(ViewController): Реализована логика взаимодействия со счетчиком

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,12 +8,53 @@
 import UIKit
 
 class ViewController: UIViewController {
+    // MARK: Properties
+    
+    var counter = Counter() {
+        didSet {
+            updateUI()
+        }
+    }
+    
+    // MARK: Outlets
 
+    @IBOutlet weak var countLabel: UILabel!
+    @IBOutlet weak var historyTextViewLabel: UITextView!
+    
+    // MARK: Life cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        updateUI()
     }
-
-
+    
+    // MARK: Private API
+    
+    private func updateUI() {
+        countLabel.text = counter.countDisplayText
+        historyTextViewLabel.text = "История изменений:\n\(counter.getHistoryDisplayText())"
+        
+        
+        let scrollY = historyTextViewLabel.contentSize.height - historyTextViewLabel.bounds.size.height
+        
+        if scrollY > 0 {
+            historyTextViewLabel.setContentOffset(.init(x: 0, y: scrollY), animated: false)
+        }
+    }
+    
+    // MARK: Actions
+    
+    @IBAction private func onClickIncrement() {
+        counter.increment()
+    }
+    
+    @IBAction private func onClickDecrement() {
+        counter.decrement()
+    }
+    
+    @IBAction private func onClickReset() {
+        counter.reset()
+    }
 }
 
